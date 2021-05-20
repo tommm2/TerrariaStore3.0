5ce1022076a534f1f37f77269b98757bf12dab9e
tomm2 <tom875694231@gmail.com>
2021-05-20 17:41:41 +0800

    新增Loading的方法

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  strict: true,
   state: {
-  },
-  mutations: {
+    isLoading: false
   },
   actions: {
+    updateLoading (context, status) {
+      context.commit('LOADING', status)
+    }
+  },
+  mutations: {
+    LOADING (state, status) {
+      state.isLoading = status
+    },
+    PRODUCTS (state, payload) {
+      state.products = payload
+    }
   },
   modules: {
   }
