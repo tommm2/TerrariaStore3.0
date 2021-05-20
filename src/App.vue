d8b4951c3e01e76be3160ce9039f401e677201e9
tomm2 <tom875694231@gmail.com>
2021-05-20 17:40:14 +0800

    將Loading元件加入

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
   <div id="app">
+    <Loading :isLoading="isLoading" />
     <router-view/>
   </div>
 </template>
+<script>
+import Loading from '@/components/Loading.vue'
+export default {
+  components: {
+    Loading
+  },
+  computed: {
+    isLoading () {
+      const vm = this
+      return vm.$store.state.isLoading
+    }
+  }
+}
+</script>
 <style lang="scss">
 @import './assets/all.scss';
 @import "./common/font/font.css";
